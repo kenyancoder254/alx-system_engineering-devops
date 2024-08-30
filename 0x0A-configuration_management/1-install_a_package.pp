7dbcdc7815932f182a081396a4bada2cf82c8945
kenyancoder254 <kenyancoder05@gmail.com>
2024-08-29 23:35:53 -0700

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,8 +2,8 @@
 package { 'python3-pip':
 ensure  =>  installed,
 }
-exec { 'install_flask':
-command => '/usr/bin/pip3 install flask==2.1.0',
-unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
-require => Package['python3-pip'],
+exec {'install_flask':
+command =>  '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.3',
+unless  =>  '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+require =>  Package['python3-pip'],
 }

c222b40b0e11a66660295a01cba4e2a3c167f9c7
kenyancoder254 <kenyancoder05@gmail.com>
2024-09-07 19:00:40 +0300

    Use puppet to configure server file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 # Configure SSH client to use private key & disable password
-file { "~/.ssh/config":
-        ensure => present,
-        content => "Host *\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no\n",
-        mode => '0600',
-}
+Host *
+  IdentityFile ~/.ssh/school
+  PasswordAuthentication no
+
